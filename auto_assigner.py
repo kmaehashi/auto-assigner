19940f830c964345c879ec9bf2b746365d193211
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-12-04 13:05:16 +0900

    add --all-pull-requests option

--- auto_assigner.py
+++ auto_assigner.py
@@ -13,7 +13,11 @@ def parse_args(args):
     parser.add_argument('--organization', type=str, required=True)
     parser.add_argument('--repo', type=str, required=True)
     parser.add_argument('--team', type=str, required=True)
-    parser.add_argument('--issue', type=int, required=True)
+    parser.add_argument('--issue', type=int, default=None)
+    parser.add_argument('--all-pull-requests', action='store_true',
+                        default=False,
+                        help='add assignee to all open Pull-Requests '
+                             '(use with care!)')
     parser.add_argument('--token', type=str, default=None)
     params = parser.parse_args(args)
 
@@ -23,6 +27,14 @@ def parse_args(args):
             raise RuntimeError(
                 '--token option or GITHUB_ACCESS_TOKEN environment'
                 'variable must be specified')
+
+    if params.issue is None and not params.all_pull_requests:
+        raise RuntimeError(
+            '--issue option or --all-pull-requests option must be specified')
+    elif params.issue is not None and params.all_pull_requests:
+        raise RuntimeError(
+            '--issue option and --all-pull-requests option cannot be '
+            'specified together')
 
     return params
 
@@ -34,6 +46,18 @@ def get_team_by_name(org, name):
     raise RuntimeError('no team named {} found'.format(name))
 
 
+def set_assignee_to_issue(candidates, issue):
+    if 0 < len(issue.assignees):
+        print('Warning: Issue/Pull-Request #{} already assigned to {}'.format(
+            params.issue, [user.login for user in issue.assignees]))
+        return
+
+    assignee = random.choice(candidates)
+    issue.edit(assignees=[assignee])
+    print('Issue/Pull-Request #{} assigned to {}'.format(
+        issue.number, assignee))
+
+
 def main(args=sys.argv[1:]):
     params = parse_args(args)
 
@@ -41,20 +65,18 @@ def main(args=sys.argv[1:]):
     org = g.get_organization(params.organization)
     repo = org.get_repo(params.repo)
 
-    issue = repo.get_issue(params.issue)
-    if 0 < len(issue.assignees):
-        # Assignee(s) are already set.
-        print('Warning: Issue/Pull-Request #{} already assigned to {}'.format(
-            params.issue, [user.login for user in issue.assignees]))
-        return
-
     team = get_team_by_name(org, params.team)
     candidates = [member.login for member in team.get_members()]
-    assignee = random.choice(candidates)
 
-    issue.edit(assignees=[assignee])
-    print('Issue/Pull-Request #{} assigned to {}'.format(
-        params.issue, assignee))
+    if params.all_pull_requests:
+        print('Getting all issues (this may take a while)...')
+        issues = repo.get_issues(state='open', assignee='none')
+        prs = [issue for issue in issues if issue.pull_request is not None]
+        for pr in prs:
+            set_assignee_to_issue(candidates, pr)
+    else:
+        issue = repo.get_issue(params.issue)
+        set_assignee_to_issue(candidates, issue)
 
 
 if __name__ == '__main__':
